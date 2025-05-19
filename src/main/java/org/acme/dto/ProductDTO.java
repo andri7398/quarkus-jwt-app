217650b62bd30249fc2685f0bd960042eb32f96f
Andri Yanto <andri.yanto7398@gmail.com>
2025-05-19 23:53:25 +0700

    adding create, update, and delete product

---
+++ src/main/java/org/acme/dto/ProductDTO.java
@@ -0,0 +1,54 @@
+package org.acme.dto;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class ProductDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Date createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
--- src/main/java/org/acme/resource/ProductResource.java
+++ src/main/java/org/acme/resource/ProductResource.java
@@ -2,13 +2,19 @@ package org.acme.resource;
 
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
+import org.acme.dto.ProductDTO;
 import org.acme.dto.SearchDTO;
 import org.acme.dto.SearchPriceDTO;
 import org.acme.model.Product;
 import org.acme.repository.ProductRepository;
 
+import java.util.Date;
 import java.util.List;
 
 @Path("/products")
@@ -18,6 +24,9 @@ import java.util.List;
 public class ProductResource {
     @Inject
     ProductRepository productRepository;
+
+    @Context
+    UriInfo uriInfo;
 
     @GET
     public List<Product> getAllProducts() {
@@ -36,11 +45,77 @@ public class ProductResource {
 
     @POST
     @Path("/searchByPriceRange")
-    public List<Product> searchProductsByName(SearchPriceDTO dto){
+    public List<Product> searchProductsByPrice(SearchPriceDTO dto){
         try {
             return productRepository.searchByPriceRange(dto.getMinPrice(), dto.getMaxPrice());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
+
+    @POST
+    @Transactional
+    @Path("/insertOrUpdateProduct")
+    public Response insertOrUpdateProduct(ProductDTO dto){
+        try {
+            Product item = new Product();
+
+            if(dto.getId() != null){
+                item = productRepository.findById(dto.getId());
+                if(item == null){
+                    return Response.status(Response.Status.NOT_FOUND)
+                            .entity("Product with id " + dto.getId() + " not found")
+                            .build();
+                }
+            }
+
+            item.setDescription(dto.getDescription());
+            item.setName(dto.getName());
+            item.setPrice(dto.getPrice());
+            item.setCreatedAt(new Date());
+
+            productRepository.persist(item);
+
+            var createdUri = uriInfo.getAbsolutePathBuilder()
+                    .path(item.getId().toString())
+                    .build();
+
+            return Response.ok(createdUri)
+                    .entity(item)
+                    .build();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @POST
+    @Transactional
+    @Path("/deleteProduct")
+    public Response deleteProduct(ProductDTO dto){
+        try {
+            Product item = new Product();
+
+            if(dto.getId() != null){
+                item = productRepository.findById(dto.getId());
+                if(item == null){
+                    return Response.status(Response.Status.NOT_FOUND)
+                            .entity("Product with id " + dto.getId() + " not found")
+                            .build();
+                }
+            }
+
+            productRepository.delete(item);
+
+            return Response.ok("data with id " + dto.getId() + " successfully deleted")
+                    .build();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
 }
