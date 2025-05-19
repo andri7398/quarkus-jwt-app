514ce1aae6227e842f48c88d96e25247ad648c2c
Andri Yanto <andri.yanto7398@gmail.com>
2025-05-19 23:07:45 +0700

    adding import sql and erasing product initializer

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,11 @@
--- This file allow to write SQL commands that will be emitted in test and dev.
--- The commands are commented as their support depends of the database
--- insert into myentity (id, field) values(1, 'field-1');
--- insert into myentity (id, field) values(2, 'field-2');
--- insert into myentity (id, field) values(3, 'field-3');
--- alter sequence myentity_seq restart with 4;
+INSERT INTO Product (name, description, price, createdAt) VALUES
+('Laptop Gaming', 'Laptop berkinerja tinggi untuk bermain game', 25000000, CURRENT_DATE),
+('Smartphone', 'Ponsel pintar dengan kamera canggih', 8000000, CURRENT_DATE),
+('Kipas Angin', 'Kipas angin hemat energi dan sunyi', 350000, CURRENT_DATE),
+('Meja Belajar', 'Meja belajar kayu jati minimalis', 1200000, CURRENT_DATE),
+('Sepeda Gunung', 'Sepeda gunung untuk segala medan', 4500000, CURRENT_DATE),
+('Kursi Kantor', 'Kursi kantor ergonomis dengan sandaran tangan', 950000, CURRENT_DATE),
+('Headphone', 'Headphone wireless dengan noise cancelling', 1500000, CURRENT_DATE),
+('Jam Tangan', 'Jam tangan digital tahan air', 600000, CURRENT_DATE),
+('Printer Inkjet', 'Printer warna untuk kebutuhan kantor', 2200000, CURRENT_DATE),
+('Backpack', 'Tas ransel tahan air dengan banyak kompartemen', 300000, CURRENT_DATE);
